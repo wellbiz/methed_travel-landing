5d18428fae8d7daa8fb8d4eb5937bd77b6c0f249
Dmitry Taras <wellbiz@ya.ru>
2022-02-07 21:07:55 +0300

    изменил date Expired на более позднюю

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import {timer} from './module/timer.js';
 
 const showTimer = () => {
     const blockTimer = document.querySelector('.timer');
-    blockTimer.dataset.timerDeadline = '2022/02/07 20:45';
+    blockTimer.dataset.timerDeadline = '2022/02/25 20:45';
     const deadline = blockTimer.dataset.timerDeadline;
     timer(deadline);
 };
