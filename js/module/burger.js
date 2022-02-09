1afb5144ce9a211b351ab7613659cb141cea89b7
Dmitry Taras <wellbiz@ya.ru>
2022-02-09 22:31:38 +0300

    добавил скрытие меню по нажатию на ссылку

--- js/module/burger.js
+++ js/module/burger.js
@@ -1,9 +1,17 @@
-// "header__menu-button" и "header__menu"
 
-// и класс "header__menu_active"
+const button = document.querySelector('.header__menu-button');
+const headerMenu = document.querySelector('.header__menu');
+button.addEventListener('click', () => {
+    headerMenu.classList.toggle('header__menu_active');
+});
 
-const button = document.querySelector(".header__menu-button");
-const headerMenu = document.querySelector(".header__menu");
-button.addEventListener("click", () => {
-  headerMenu.classList.toggle("header__menu_active");
+const headerList = document.querySelector('.header__list');
+
+headerList.addEventListener('click', (e) => {
+    if (e.target.matches('.header__link'))
+        headerMenu.classList.toggle('header__menu_active');
+});
+headerMenu.addEventListener('mouseleave', (e) => {
+    
+        headerMenu.classList.remove('header__menu_active');
 });
