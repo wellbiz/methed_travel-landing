4fa53c26c880078242c22eefd931f1a3986505fa
Parit <wellbiz@ya.ru>
2022-02-10 10:16:02 +0300

    Create burger.js
    обновил документ, повесил обработчик на весь документ.

--- js/module/burger.js
+++ js/module/burger.js
@@ -1,9 +1,15 @@
-// "header__menu-button" и "header__menu"
+const closeMenuByOutMenu = () => {
+  const headerMenu = document.querySelector(".header__menu");
 
-// и класс "header__menu_active"
+  document.addEventListener("click", (e) => {
+    const target = e.target;
+    //если кликаем по кнопке, то переключаем класс
+    if (target.classList.contains("header__menu-button")) {
+      headerMenu.classList.toggle("header__menu_active");
+      //если клик вне меню, то закрываем его
+    } else if (!target.classList.contains("header__menu"))
+      headerMenu.classList.remove("header__menu_active");
+  });
+};
 
-const button = document.querySelector(".header__menu-button");
-const headerMenu = document.querySelector(".header__menu");
-button.addEventListener("click", () => {
-  headerMenu.classList.toggle("header__menu_active");
-});
+closeMenuByOutMenu();
