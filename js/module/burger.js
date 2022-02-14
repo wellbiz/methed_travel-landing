3260325c375232a7a9aaf10053a2fa3d9daff062
Dmitry Taras <wellbiz@ya.ru>
2022-02-14 23:34:19 +0300

    чуть подправил анимацию бургера

--- js/module/burger.js
+++ js/module/burger.js
@@ -5,9 +5,16 @@ const closeMenuByOutMenu = () => {
         const target = e.target;
         //если кликаем по кнопке, то переключаем класс
         if (target.classList.contains('header__menu-button')) {
-            showMenu();
+            toggleMenu();
+            console.log('togglemenu');
             //если клик вне меню, то закрываем его
-        } else if (!target.classList.contains('header__menu')) hideMenu();
+        } else if (!target.classList.contains('header__menu')) {
+            hideMenu();
+            console.log(`hidemenu`);
+        } else {
+            showMenu();
+            console.log(`showmenu`);
+        }
         //дальше нам не надо ловить события...
         e.stopPropagation();
     });
@@ -15,26 +22,42 @@ const closeMenuByOutMenu = () => {
 
 closeMenuByOutMenu();
 
-let opacity = 1;
+let opacityHideMenu = 1;
 const hideMenu = () => {
     const menu = document.querySelector('.header__menu');
 
-    opacity -= 0.05;
-    menu.style.opacity = opacity;
-    if (opacity > 0) {
+    opacityHideMenu -= 0.1;
+    menu.style.opacity = opacityHideMenu;
+    if (opacityHideMenu > 0) {
         requestAnimationFrame(hideMenu, 10);
     } else {
         menu.classList.remove('header__menu_active');
     }
 };
 
-opacity = 0;
+let opacityToggleMenu = 0;
+const toggleMenu = () => {
+    const menu = document.querySelector('.header__menu');
+
+    opacityToggleMenu += 0.1;
+    menu.style.opacity = opacityToggleMenu;
+
+    if (opacityToggleMenu < 1) {
+        requestAnimationFrame(toggleMenu, 10);
+    }
+    else if (menu.classList.contains('header__menu_active')) {
+        menu.classList.remove('header__menu_active');
+    } else {
+        menu.classList.add('header__menu_active');
+    }
+};
+let opacityShowMenu = 0;
 const showMenu = () => {
     const menu = document.querySelector('.header__menu');
 
-    opacity += 0.05;
-    menu.style.opacity = opacity;
-    if (opacity < 1) {
+    opacityShowMenu += 0.1;
+    menu.style.opacity = opacityShowMenu;
+    if (opacityShowMenu < 1) {
         requestAnimationFrame(showMenu, 10);
     } else {
         menu.classList.add('header__menu_active');
